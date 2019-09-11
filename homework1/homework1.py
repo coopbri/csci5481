8a9f52a7072a32d95aec82bb4f0d414a34450149
brian <bcoopmn@gmail.com>
2019-09-10 20:23:56 -0500

    added BURST execution, command argument construction

--- homework1/homework1.py
+++ homework1/homework1.py
@@ -8,6 +8,7 @@ import sys, os
 import argparse
 from subprocess import Popen, PIPE
 
+# Setup and establish command line arguments
 def make_arg_parser():
     parser = argparse.ArgumentParser(prog='homework1.py',
                           formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -40,12 +41,12 @@ def make_arg_parser():
 def run_burst(query, ref, taxonomy, output, burst_cmd='./burst', verbose=False):
     """thread worker function"""
 
-    ### YOUR CODE HERE ###
+    # Construct command parameters for BURST based on command line input
+    cmd = f"{burst_cmd} -r {ref} -q {query} --taxonomy {taxonomy} -o {output}"
 
-    print(cmd)
     return run_command(cmd, verbose=verbose)
 
-# runs the given command and returns return value and output
+# Runs the given command and returns return value and output
 def run_command(cmd, verbose=False):
     if verbose:
         print(cmd)
@@ -59,5 +60,6 @@ if __name__ == '__main__':
     parser = make_arg_parser()
     args = parser.parse_args()
 
-    ### YOUR CODE HERE ###
-    run_burst(args.query, args.ref, args.taxonomy, args.output, args.command, args.verbose)
+    # Execute the BURST program with provided command-line arguments
+    run_burst(args.query, args.ref, args.taxonomy, args.output, args.command,
+              args.verbose)
