7e37c03670ac1dc74773d61ffaed466a04445516
brian <bcoopmn@gmail.com>
2019-09-13 14:15:39 -0500

    added regex for question 4b (WIP), code formatting

--- homework1/q4.py
+++ homework1/q4.py
@@ -2,23 +2,43 @@
 #   Brian Cooper
 #   CSCI 5481 at University of Minnesota
 
+# regex library (Question 4c)
+import re
+
 if __name__ == '__main__':
-    # output file
+    # Question 4a: fraction of original input query sequences at 97+ percent
+    # output file, line-by-line
     outfile = open("output.txt").readlines()
 
     # store all values above threshold (97 percent similarity)
     data = [float(n.split()[2]) for n in outfile if float(n.split()[2]) >= 97.0]
 
-    # Question 4a: fraction of original input query sequences at 97+ percent
     queryMatch = len(data) / 130727
     print("Question 4a")
     print("===========")
-    print(f"Sequences that match database at 97%+ similarity:    {round(queryMatch*100, 4)}%\n")
+    print(f"Sequences that match database at 97%+ similarity: \
+    {round(queryMatch*100, 4)}%\n")
+
+    # ==========================================================================
 
     # Question 4b: most common bacterial species
+    # open output file as read-only
+    outfile = open("output.txt", "r")
+
+    pattern="s__(.*)"
+    match = re.match(pattern, outfile.read())
+    print("Question 4b")
+    print("===========")
+    print(f"{match}\n")
+
+    # ==========================================================================
 
     # Question 4c: average percent similarity of matches
     avg = sum(data) / len(data)
     print("Question 4c")
     print("===========")
-    print(f"Average percent similarity of matches at 97%+ similarity:    {round(avg, 4)}%\n")
+    print(f"Average percent similarity of matches at 97%+ similarity: \
+    {round(avg, 4)}%\n")
+
+    # close output file
+    outfile.close()
