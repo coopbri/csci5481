f4f95cdaf45094cee4fa672a3b91fadfca9d9beb
brian <brian@brian-cooper.com>
2019-09-30 19:16:36 -0500

    WIP N-W alignment

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -61,68 +61,82 @@ def create_matrix(q, r):
 
     # Fill matrix with scores
     for i in range(1, len(q)+1):
-        # Determine match or mismatch
-        if (q[i-1] == r[j-1]):
-            diag = V[i-1][j-1] + MATCH
-        else:
-            diag = V[i-1][j-1] + MISMATCH
+        for j in range(1, len(r)+1):
+            # Determine match or mismatch
+            if (q[i-1] == r[j-1]):
+                diag = V[i-1][j-1] + MATCH
+            else:
+                diag = V[i-1][j-1] + MISMATCH
 
-        left = V[i][j-1] + GAP
-        above = V[i-1][j] + GAP
+            left = V[i][j-1] + GAP
+            above = V[i-1][j] + GAP
 
-        V[i][j] = max(diag, left, above)
+            V[i][j] = max(diag, left, above)
 
     return V
-
 
 # ============================================================================ #
 # Needleman-Wunsch algorithm                                                   #
 #   (param) V: similarity matrix                                               #
 # ============================================================================ #
-def needlemanWunsch(V):
+def needleman_wunsch(V):
     # Initialize alignments as empty strings
     alignQ = ""
     alignR = ""
 
-    finalScore = V[len(q)-1][len(r)-1]
-
     i = len(q)
     j = len(r)
 
+    finalScore = V[i][j]
+
     while i > 0 and j > 0:
+        # Determine scores in proximity
         curScore = V[i][j]
-        diagScore = V[i-1][j-1]
-        leftScore = V[i][j-1]
-        aboveScore = V[i-1][j]
 
-        if (curScore == diagScore + MATCH) or (curScore == diagScore + MISMATCH):
-            alignQ += q[i-1]
-            alignR += r[j-1]
-            i--
-            j--
-        elif curScore == leftScore + GAP:
-            alignQ += "-"
-            alignR += r[j-1]
-            j--
-        elif curScore == aboveScore + GAP:
-            alignQ += q[i-1]
-            alignR += "-"
-            i--
+        if (i > 0 and j > 0) and ((curScore == V[i-1][j-1] + MATCH) or (curScore == V[i-1][j-1] + MISMATCH)):
+            alignQ = q[i-1] + alignQ
+            alignR = r[j-1] + alignR
+            i -= 1
+            j -= 1
+        elif curScore == V[i-1][j] + GAP:
+            alignQ = q[i-1] + alignQ
+            alignR = "-" + alignR
+            i -= 1
+        else:
+            alignQ = "-" + alignQ
+            alignR = r[j-1] + alignR
+            j -= 1
 
-    # CHECK
-	while i > 0:
-		alignQ += q[i-1]
-		alignR = "-"
-		i--
-	while j > 0:
-		alignQ += "-"
-		alignR = r[j-1]
-		j--
+    # while i > 0:
+    #     alignQ = q[i-1] + alignQ
+    #     alignR = "-" + alignR
+    #     i -= 1
+    # while j > 0:
+    #     alignQ = "-" + alignQ
+    #     alignR = r[j-1] + alignR
+    #     j -= 1
+
+    return alignQ, alignR, finalScore
 
 # ============================================================================ #
 # Main function                                                                #
 # ============================================================================ #
 if __name__ == "__main__":
+    # Handle command line parameters
+    parser = make_arg_parser()
+    args = parser.parse_args()
+
     # Read FASTA sequence files (query and reference)
-    # q =
-    # r =
+    q = read(args.query)
+    r = read(args.reference)
+
+    # Create and fill similarity matrix for Needleman-Wunsch
+    V = create_matrix(q, r)
+
+    # Perform Needleman-Wunsch algorithm
+    alignQ, alignR, score = needleman_wunsch(V)
+
+    # Output alignments and score
+    print("Query alignment:     " + alignQ)
+    print("Reference alignment: " + alignR)
+    print("\nFinal score: " + str(score))
