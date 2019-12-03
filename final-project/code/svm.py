feca1eb1ac4b4de4aae95fffc4e05f953835749e
brian <brian@brian-cooper.com>
2019-12-02 18:43:15 -0600

    clean formatting

--- final-project/code/svm.py
+++ final-project/code/svm.py
@@ -1,3 +1,7 @@
+# CSCI 5481 Final Project
+# University of Minnesota
+# Code by Brian Cooper
+# This file sets up the quantum and classical support vector machines.
 from datasets import *
 import numpy as np
 from qiskit.aqua.utils import split_dataset_to_data_and_labels
@@ -5,13 +9,12 @@ from qiskit.aqua.input import ClassificationInput
 from qiskit.aqua import run_algorithm
 # from qiskit_qcgpu_provider import QCGPUProvider
 
-
-# ======================================================
-# Quantum Support Vector Machine
-# train_input:  training set
-# test_input:   testing set
-# n:            projected input dimensionality
-# ======================================================
+# ============================================================================ #
+# Quantum support vector machine                                               #
+#   train_input:  training set                                                 #
+#   test_input:   testing set                                                  #
+#   n:            projected input dimensionality                               #
+# ============================================================================ #
 def quantum_svm(train_input, test_input, class_labels, n):
     temp = [test_input[k] for k in test_input]
     total_array = np.concatenate(temp)
@@ -23,7 +26,8 @@ def quantum_svm(train_input, test_input, class_labels, n):
             'algorithm': {
                 'name': 'QSVM'
             },
-            'feature_map': {'name': 'SecondOrderExpansion', 'depth': 2, 'entangler_map': [[0, 1]]},
+            'feature_map': {'name': 'SecondOrderExpansion', 'depth': 2, \
+                'entangler_map': [[0, 1]]},
             'multiclass_extension': {'name': 'AllPairs'},
             'backend': {'name': 'qasm_simulator', 'shots': 256}
         }
@@ -33,14 +37,15 @@ def quantum_svm(train_input, test_input, class_labels, n):
             'algorithm': {
                 'name': 'QSVM'
             },
-            'feature_map': {'name': 'SecondOrderExpansion', 'depth': 2, 'entangler_map': [[0, 1]]},
+            'feature_map': {'name': 'SecondOrderExpansion', 'depth': 2, \
+                'entangler_map': [[0, 1]]},
             'backend': {'name': 'qasm_simulator', 'shots': 256}
         }
 
     # Use the QCGPUProvider for improved performance
     # If using, make sure to enable the import code on line 6
-#     backend = QCGPUProvider().get_backend('qasm_simulator')
-        
+    # backend = QCGPUProvider().get_backend('qasm_simulator')
+
     algo_input = ClassificationInput
     algo_input.training_dataset = train_input
     algo_input.test_dataset = test_input
@@ -49,25 +54,24 @@ def quantum_svm(train_input, test_input, class_labels, n):
     # Run the quantum SVM algorithm
     # Use the second version if using the QCGPUProvider backend (GPU)
     result = run_algorithm(aqua_dict, algo_input)
-#     result = run_algorithm(aqua_dict, algo_input, backend=backend)
-    
+    # result = run_algorithm(aqua_dict, algo_input, backend=backend)
+
     # Print model values
-#     for k,v in result.items():
-#         print("'{}' : {}".format(k, v))
-    
+    # for k,v in result.items():
+    #   print("'{}' : {}".format(k, v))
+
     return result
 
-
-# ======================================================
-# Classical Support Vector Machine (Radial Basis Kernel)
-# train_input:  training set
-# test_input:   testing set
-# n:            projected input dimensionality
-# ======================================================
+# ============================================================================ #
+# Classical support vector machine (Radial basis kernel)                       #
+#   train_input:  training set                                                 #
+#   test_input:   testing set                                                  #
+#   n:            projected input dimensionality                               #
+# ============================================================================ #
 def classical_svm(train_input, test_input, class_labels, n):
     temp = [test_input[k] for k in test_input]
     total_array = np.concatenate(temp)
-    
+
     # Select parameters based on number of classes in data
     if len(class_labels) > 2:
         aqua_dict = {
@@ -92,9 +96,9 @@ def classical_svm(train_input, test_input, class_labels, n):
 
     # Run the classical SVM algorithm
     result = run_algorithm(aqua_dict, algo_input)
-    
+
     # Print model values
-#     for k,v in result.items():
-#         print("'{}' : {}".format(k, v))
-    
+    #  for k,v in result.items():
+    #      print("'{}' : {}".format(k, v))
+
     return result
